1439fba2fd9789261db90ca14ae26cc66828abc7
Force <force@ip-172-31-27-143.ap-southeast-1.compute.internal>
2017-07-08 14:18:40 +0000

    Add Json respone

--- kshamata/administration/urls.py
+++ kshamata/administration/urls.py
@@ -18,5 +18,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'', views.index, name="index"),
+    url(r'$', views.index, name="index"),
+    url(r'phptest/$', views.phptest, name="php"),
 ]
--- kshamata/administration/views.py
+++ kshamata/administration/views.py
@@ -2,9 +2,17 @@
 from __future__ import unicode_literals
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+# from django.views.decorators.csrf import crf_exempt
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+    if request.POST is True:
+	print(request.POST)
+    return JsonResponse({"count":1})
+
+
+def phptest(request):
+    return render(request,"phpAndroidAccess.php")    
