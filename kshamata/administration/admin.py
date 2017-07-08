e143ae197fa8015a0b3a486924bfb944f1ba9015
Sahil Sheikh <sheikhsahil549@gmail.com>
2017-07-08 18:14:42 +0530

    add admin

--- kshamata/administration/admin.py
+++ kshamata/administration/admin.py
@@ -3,4 +3,33 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+
+from .models import PersonalInfo, TrainingDetails, PlacementDetails, shelterlife, activity
+
+
 # Register your models here.
+
+admin.site.register(PersonalInfo)
+admin.site.register(TrainingDetails)
+admin.site.register(PlacementDetails)
+admin.site.register(shelterlife)
+admin.site.register(activity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
