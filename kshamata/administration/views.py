d7839d998159244f5645432568e072d83593b9fc
Manoj Balaji <manojbalaji1@gmail.com>
2017-07-08 21:20:34 +0530

    fix login

--- kshamata/administration/views.py
+++ kshamata/administration/views.py
@@ -3,16 +3,31 @@ from __future__ import unicode_literals
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
-# from django.views.decorators.csrf import crf_exempt
-
+from django.contrib.auth import (
+    login,
+    logout,
+    get_user_model,
+    user_logged_in,
+    user_logged_out,
+    user_login_failed,
+    authenticate,
+)
 # Create your views here.
 
 
 def index(request):
-    if request.POST is True:
-	print(request.POST)
-    return JsonResponse({"count":1})
+    return JsonResponse({
+        "count": 1,
+    })
 
 
-def phptest(request):
-    return render(request,"phpAndroidAccess.php")    
+def login(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        print(user_logged_in)
+    else:
+        print user_login_failed
+    return 1
+
