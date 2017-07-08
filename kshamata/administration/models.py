39a89634a37cfb7eef8e47028d002d87c696bdb8
Sahil Sheikh <sheikhsahil549@gmail.com>
2017-07-08 17:12:01 +0530

    Add placement details into models

--- kshamata/administration/models.py
+++ kshamata/administration/models.py
@@ -19,3 +19,44 @@ class TrainingDetails(models.Model):
     life_skills = models.CharField(max_length=40)
     vocational_skills = models.CharField(max_length=200)
     further_education = models.CharField(max_length=100)
+
+
+class PlacementDetails(models.Model):
+	uid = models.OneToOneField(PersonalInfo)
+	currentemployer = models.CharField(max_length=40)
+	salary = models.IntegerField(max_length = 6)
+	typeofemployment = 	models.CharField(max_length=40)
+	satisfied = models.BooleanField(default = True)
+	savings = models.IntegerField(max_length = 6)
+	expectations = models.CharField(max_length=100)
+
+
+'''class shelterlife(models.Model):
+	uid = models.OneToOneField(PersonalInfo)
+	#location
+	shared = models.Boolean()
+	avgexpense = models.IntegerField(max_length = 6)
+	savings = models.IntegerField(max_length = 6)
+	#relation with roommates
+	#relation with neighbours
+
+
+class activity(models.Model):
+	uid = models.OneToOneField(PersonalInfo)
+	institution = models.CharField(max_length = 20)
+	startdate = models.DateField(auto_now_add = True,auto_now = False)
+	enddate = models.DateField(auto_now_add = True,auto_now = False)
+	#duration 
+	natureofactivity =  models.CharField(max_length=100)
+	noofwomen =  models.IntegerField(max_length = 100)
+	expectedoutcome = models.CharField(max_length=100)
+	obtainedoutcome = models.CharField(max_length=100)
+	amountspend = models.IntegerField(max_length = 6)'''
+	
+
+
+
+
+
+    
+
