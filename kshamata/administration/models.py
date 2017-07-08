4639c6310966fc4e4e36433a44cdfd12c97da3d1
Manoj Balaji <manojbalaji1@gmail.com>
2017-07-08 16:17:20 +0530

    Add PersonalInfo in models.py at administration app

--- kshamata/administration/models.py
+++ kshamata/administration/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PersonalInfo(models.Model):
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(max_length=3)
+    photo = models.ImageField(upload_to="profile/img")
+    education_level = models.CharField(max_length=30)
+    date_since_joining = models.DateField(auto_now_add=True, auto_now=False)
